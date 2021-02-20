57d9da54512932d67196003a77476a142b13de27
WT <wt0881@my.bristol.ac.uk>
2021-02-20 16:05:14 +0000

    Add script to run all timing experiments

--- scripts/run_benchmarking_experiments.sh
+++ scripts/run_benchmarking_experiments.sh
@@ -3,5 +3,3 @@ julia scripts/pseudo_point_synthetic_experiments_regular_obs.jl --run-experiment
 julia scripts/pseudo_point_synthetic_posterior_plots.jl
 julia scripts/pseudo_point_synthetic_experiments_irregular_obs_sum_separable.jl --run-experiments=true
 julia scripts/pseudo_point_synthetic_experiments_regular_obs_sum_separable.jl --run-experiments=true
-julia scripts/global_historical_climatology_network.jl --run-experiments=true
-julia scripts/apartment_prices.jl --run-experiments=true
